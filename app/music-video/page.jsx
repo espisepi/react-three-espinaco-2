0d8a51a32f1f2038a523a52207ed5708ca184663
espisepi <sepinaco@gmail.com>
2023-06-04 15:10:42 +0200

    /music-video: add video html tag with mcpi video

--- app/music-video/page.jsx
+++ app/music-video/page.jsx
@@ -27,16 +27,13 @@ export default function Page() {
   return (
     <>
       <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
-        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
-        </div>
+        <video id="video" style={{ width: '25vw', height: '25vh', top: 0, zIndex: 100, position: 'absolute' }}
+          src={'videos/mcpi.mp4'} controls={true} autoPlay={true} crossOrigin="anonymous"></video>
       </div>
 
       <View className='absolute top-0 flex h-screen w-full flex-col items-center justify-center'>
         <VideoPoints />
-        <Common />
+        <Common  />
       </View>
     </>
   )
