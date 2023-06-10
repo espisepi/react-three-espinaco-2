8ff89af7e0c0c3b9424145e006016b8806d410d6
espisepi <sepinaco@gmail.com>
2023-06-10 22:58:44 +0200

    /sketchbook: button left-arrow added and working fine! :)

--- src/features/sketchbook/SketchbookUI.jsx
+++ src/features/sketchbook/SketchbookUI.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useRef, useState } from "react";
+import styles from './SketchbookUI.module.css';
 
+// Estilos
+const buttonLeftArrowStyle = [ styles['button'], styles['left-arrow'] ].join(' ');
 
 
 export const SketchbookUI = () => {
+    // ====================================================
+    // Se hace de esta manera porque el div que se muestra no es el de por defecto (es el uiContainerEl el que se muestra),
+    // por eso lo inyectamos con appendChild
     const ref = useRef();
     const [uiContainerEl, setUiContainerEl] = useState();
     useEffect(()=>{
@@ -22,17 +28,30 @@ export const SketchbookUI = () => {
     },[ref]);
     useEffect(()=>{
         if(ref.current && uiContainerEl) {
-            console.log(ref);
-            console.log(uiContainerEl);
             uiContainerEl.appendChild(ref.current);
         }
     },[ref, uiContainerEl]);
+    // =========================================================================
+    // Attach key input controls with onclick buttons
+    const handlePointerDownLeftArrow = (ev) => {
+        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', code: 'KeyA' }));
+    }
+    const handlePointerUpLeftArrow = (ev) => {
+		document.dispatchEvent(new KeyboardEvent('keyup', { key: 'a', code: 'KeyA' }));
+    }    
+
 
     return (
         <>
         <div ref={ref} id='sketchbook-ui' style={{ backgroundColor:'red', position: 'absolute',zIndex:999}}>
             <h1>OYEEEE</h1>
-            <button onClick={(ev)=>alert('Holi!')}>OYEE</button>
+            {/* <button onClick={(ev)=>alert('Holi!')}>OYEE</button> */}
+            <button
+                className={buttonLeftArrowStyle}
+                onPointerDown={(ev)=>handlePointerDownLeftArrow(ev)}
+                onPointerUp={(ev)=>handlePointerUpLeftArrow(ev)}
+                onPointerOut={(ev)=>handlePointerUpLeftArrow(ev)}
+            ></button>
         </div>
         </>
     )
